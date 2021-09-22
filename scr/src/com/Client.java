a71dea5230bd11456356aec096e7bbac909389d9
valesskablanco <valesskablanco2@gmail.com>
2021-09-21 21:09:40 -0600

    Idea Cliente Servidor

--- scr/src/com/Client.java
+++ scr/src/com/Client.java
@@ -1,71 +1,49 @@
 package com;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Client {
+    private boolean right;
 
-    private static final String ip = "127.0.0.1";
-    private static final int puerto = 9090;
-    //public static GUI frame;
+    public Client(){
+        try {
+            Socket clientReciever = new Socket("127.0.0.1", 8080);
+            Infopack inPack;
 
-    public Client() throws IOException {
-        this.runClient();
-    }
-
-    public Client(String s) throws IOException {
-        if(s.equals("Server") || s.equals("server")) {
-            this.runServer();
-        }
-        this.runClient();
-    }
-
-    public void runServer() throws IOException {
-
-        ServerSocket server = new ServerSocket(puerto);
-
-        while (true) {
-
-            System.out.println("[SERVER] Esperando conexón...");
-            Socket client = server.accept();
-            System.out.println("[SERVER] Cliente conectado.");
-            ClientHandler clientThread = new ClientHandler(client);
-            Server.clients.add(clientThread);
-
-            Server.pool.execute(clientThread);
-
+            while (true) {
+                ObjectInputStream in = new ObjectInputStream(clientReciever.getInputStream());
+                inPack = (Infopack) in.readObject();
+                right = inPack.isRight();
+                clientReciever.close();
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 
-    public void runClient() throws IOException {
+    public void send (int correct, int answer){
+        try {
+            Socket clientSender = new Socket("127.0.0.1", 9090);
+            Infopack num = new Infopack();
+            num.setRight(false);
+            num.setCorrect(correct);
+            num.setAnswer(answer);
+            ObjectOutputStream out = new ObjectOutputStream(clientSender.getOutputStream());
+            out.writeObject(num);
+            clientSender.close();
 
-        //frame = new GUI();
-
-        Socket socket = new Socket(ip, puerto);
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-
-        while (true) {
-
-            System.out.println("> Indique los datos del producto: valor.porcentaje.peso ");
-            String datos = keyboard.readLine();
-
-            if (datos.equals("quit")) break;
-
-            out.println(datos);
-
-            String monto = in.readLine();
-            System.out.println("El valor del monto es: " + monto);
-
+        } catch (Exception e){
+            System.out.println(e.getMessage());
         }
+    }
 
-        socket.close();
-
+    public void setRight(boolean right){
+        this.right = right;
+    }
+    public boolean isRight(){
+        return right;
     }
 }
--- scr/src/com/Server.java
+++ scr/src/com/Server.java
@@ -1,16 +1,42 @@
 package com;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Server {
 
-    public static final ArrayList<ClientHandler> clients = new ArrayList<>();
-    public static final ExecutorService pool = Executors.newFixedThreadPool(2);
+    public static void main (String[] args){
+
+        try {
+            int port = 9090;
+            ServerSocket server = new ServerSocket(port);
+            System.out.println("Servidor Inicializado");
+            Infopack inPack;
+
+            while (true){
+                Socket clientS = server.accept(); //esperando conexion
+                ObjectInputStream in = new ObjectInputStream(clientS.getInputStream());
+                inPack = (Infopack) in.readObject();
+
+                inPack.setRight(inPack.getCorrect() == inPack.getAnswer());
+
+                System.out.println(inPack.isRight());
+                Socket output= new Socket("127.0.0.1", 8080);
+                ObjectOutputStream out = new ObjectOutputStream(output.getOutputStream());
+                out.writeObject(inPack);
+
+                clientS.close();
+                output.close();
+            }
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
 
 }
